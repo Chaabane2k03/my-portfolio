e4319d801347e4d8729b9b2453edb971167a0732
Chaabane2k03 <chaabaneboussadia@gmail.com>
2025-02-19 08:35:46 +0100

    testting backend in NextJs

--- src/app/admin/admin_pages/SkillsPage.jsx
+++ src/app/admin/admin_pages/SkillsPage.jsx
@@ -1,9 +1,17 @@
+"use client";
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const SkillsPage = () => {
-  return (
-    <div>SkillsPage</div>
-  )
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return <div>{message}</div>;
 }
 
 export default SkillsPage
